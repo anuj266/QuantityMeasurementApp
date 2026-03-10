f29ab9100564e1ac3af5a40bbf1f906e8b79468f
anuj266 <anujudaywal@gmail.com>
2026-03-10 16:29:31 +0530

    [Anuj Udaywal] Add.Added UC11

---
+++ src/main/java/com/anuj/quantityMeasurement/VolumeUnit.java
@@ -0,0 +1,26 @@
+package com.anuj.quantityMeasurement;
+
+public enum VolumeUnit implements IMeasureable {
+	MILLILITRE(0.001),
+	LITRE(1.0),
+	GALLON(3.78541);
+	
+	private final double conversionFactor;
+	
+	VolumeUnit(double conversionFactor){
+		this.conversionFactor=conversionFactor;
+	}
+	
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
+	
+	public double convertToBaseUnit(double value) {
+		return value*getConversionFactor();
+	}
+	
+	public double convertFromBaseUnit(double value) {
+		return value/getConversionFactor();
+	}
+	
+}
