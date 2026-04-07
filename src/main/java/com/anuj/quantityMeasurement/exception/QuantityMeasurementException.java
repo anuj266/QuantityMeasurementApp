e3471c79e04822fad0808932f8aacc764d4c926e
anuj266 <anujudaywal@gmail.com>
2026-04-07 23:08:59 +0530

    [Anuj Udaywal] Add.Added UC17 , Integration of spring framework

---
+++ src/main/java/com/anuj/quantityMeasurement/exception/QuantityMeasurementException.java
@@ -0,0 +1,17 @@
+package com.anuj.quantityMeasurement.exception;
+
+public class QuantityMeasurementException extends RuntimeException {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public QuantityMeasurementException(String message) {
+        super(message);
+    }
+
+    public QuantityMeasurementException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
